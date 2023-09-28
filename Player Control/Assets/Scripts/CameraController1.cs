a99883394daf96be057e87a56ba4a15dca5f27e7
RzrRacer88 <Georgellamosas@hotmail.com>
2023-09-28 14:26:46 -0700

    Updating game files

---
+++ Player Control/Assets/Scripts/CameraController1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController1 : MonoBehaviour
+{
+    public GameObject ObjectToFollow;
+
+    private Vector3 _offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - ObjectToFollow.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = ObjectToFollow.transform.position + _offset;
+    }
+}
